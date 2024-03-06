357cb9ea645801369a804cf8f28426d4b4d82225
Ruchi Sharma <sharma101ruchi@gmail.com>
2024-03-06 23:18:57 +0530

    temp test for serverless error fix

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+// import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 import {SuccessResponse, DataErrorResponse, ErrorResponse} from "../controllers/base_response.js";
@@ -16,13 +16,13 @@ export const register  = async (req , res) => {
 
         const user = await User.findOne({ email : email });
         if (user) return DataErrorResponse(res, "User already exist with the entered email", {});;
-        const salt = await bcrypt.genSalt();
-        const passwordHash = await bcrypt.hash(password , salt);
+        // const salt = await bcrypt.genSalt();
+        // const passwordHash = await bcrypt.hash(password , salt);
 
         const newUser = User({
             name, 
             email,
-            password : passwordHash 
+            password  
       
              });
         // res.status(200).json({msg : "Account created"})
@@ -42,12 +42,12 @@ export const login = async (req, res) => {
         const user = await User.findOne({ email : email });
         if (!user) return DataErrorResponse(res, "Email does not exist", {});;
 
-        const isMatch = await bcrypt.compare(password , user.password);
-        if(!isMatch) return DataErrorResponse(res, "Invalid Password", {} );;
+        // const isMatch = await bcrypt.compare(password , user.password);
+        // if(!isMatch) return DataErrorResponse(res, "Invalid Password", {} );;
         
         
         const token = jwt.sign({ id: user.id } , process.env.JWT_SECRET);
-        delete user.password;
+        // delete user.password;
         return SuccessResponse(res, "Logged In successfully" ,{ token, user });
     }
     catch(e){
